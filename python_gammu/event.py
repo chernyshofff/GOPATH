75fee153cf4b1e5d814c4bf136b477e0767dd205
chernyshofff <kchernyshof@gmail.com>
2021-10-20 15:17:25 +0500

    Create event loader

---
+++ python_gammu/event.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+class MyHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        print ("on created", event.src_path)
+
+event_handler = MyHandler()
+observer = Observer ()
+observer.schedule(event_handler, path='./inbox', recursive=False)
+observer.start()
+
+while True:
+    try:
+        pass
+    except KeyboardInterrupt:
+        observer.stop()
